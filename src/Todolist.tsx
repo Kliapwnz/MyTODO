4501484c2f31c57e41f8bcbaee3c420fd870df75
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-29 23:46:39 +0300

    add enter

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -24,10 +24,10 @@ export function Todolist(props: PropsType) {
             const removeTaskHandler = () => {
                 props.removeTask(t.id)
             }
+
+
             return (
                 <li key={t.id}>
-                    {/*<button onClick={removeTaskHandler}>x*/}
-                    {/*</button>*/}
                     <Button buttonName={"x"} callBack={removeTaskHandler}/>
                     <input type="checkbox" checked={t.isDone}/>
                     <span>{t.title}</span>
@@ -47,29 +47,15 @@ export function Todolist(props: PropsType) {
         }
     }
 
-    const changeFilterHandlerAll = () => {
-        props.changeFilter("all")
-    }
-    const changeFilterHandlerActive = () => {
-        props.changeFilter("active")
-    }
-    const changeFilterHandlerCompleted = () => {
-        props.changeFilter("completed")
-    }
-
     const changeFilterTsarHandler = (filterValue: FilterValuesType) => {
         props.changeFilter(filterValue)
     }
-    //
-    // const removeTaskHandler = (tId:string) => {
-    //     props.removeTask(tId)
-    // }
 
     return <div>
         <h3>{props.title}</h3>
         <div>
             <input onChange={onChangeHandler} value={title} onKeyDown={onKeyDownHandler}/>
-            {/*<button onClick={addTaskHandler}>+</button>*/}
+
             <Button buttonName={"+"} callBack={addTaskHandler}/>
         </div>
         <ul>
@@ -79,9 +65,5 @@ export function Todolist(props: PropsType) {
             <Button buttonName={"All"} callBack={() => changeFilterTsarHandler("all")}/>
             <Button buttonName={"Active"} callBack={() => changeFilterTsarHandler("active")}/>
             <Button buttonName={"Completed"} callBack={() => changeFilterTsarHandler("completed")}/>
-            {/*<button onClick={() => changeFilterTsarHandler("all")}>All</button>*/}
-            {/*<button onClick={() => changeFilterTsarHandler("active")}>Active</button>*/}
-            {/*<button onClick={() => changeFilterTsarHandler("completed")}>Completed</button>*/}
-        </div>
-    </div>
+        </div></div>
 }
