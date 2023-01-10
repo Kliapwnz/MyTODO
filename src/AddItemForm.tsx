17ecf4b0353e93a0467b8a189a7bab1b2d65ebc0
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-10 18:39:21 +0300

    try to universal

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-export const AddItemForm = () => {
+type PropsType={
+    addItem:(title:string)=>void
+}
+
+export const AddItemForm = (props:PropsType) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.addItem(newTitle, props.id);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
     return (
         <div>
-
+            <div>
+                <input value={title}
+                       onChange={onChangeHandler}
+                       onKeyPress={onKeyPressHandler}
+                       className={error ? "error" : ""}
+                />
+                <button onClick={addTask}>+</button>
+                {error && <div className="error-message">{error}</div>}
+            </div>
         </div>
     );
 };
